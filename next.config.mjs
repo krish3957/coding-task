a5e719e163e780cc7bd0489e6cbe66b213a425e6
krish3957 <kpparekh81602@gmail.com>
2024-03-06 16:10:33 +0530

    Update serverActions configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverActions: true
-    }
-};
+        serverActions: {
+            allowedOrigins: ['/'],
+        },
+    },
+}
 
 export default nextConfig;
